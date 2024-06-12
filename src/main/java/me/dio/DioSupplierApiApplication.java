e7edc1167583772f2fdbe8f5abdd8e30fabfec9b
pafesan <pablof9610@gmail.com>
2024-06-11 23:27:20 -0300

    initial commit

---
+++ src/main/java/me/dio/DioSupplierApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSupplierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSupplierApiApplication.class, args);
+	}
+
+}
